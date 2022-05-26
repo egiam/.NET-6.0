68a3cb5f09f9e923f88fd775c5042b7f0af25edb
egiam <ezegiampaoli@gmail.com>
2022-05-26 12:36:26 -0300

    Update del Bingo

--- CrearTarjetasBingo/CrearTarjetasBingo/Program.cs
+++ CrearTarjetasBingo/CrearTarjetasBingo/Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Intento de creacion de carton de bingo
-//
 
 //Generar un programa que cree un cartón de bingo aleatorio y lo muestre por pantalla
 
@@ -12,45 +11,34 @@
 //6)    La primer columna contiene los números del 1 al 9, la segunda del 10 al 19, la tercera del 20 al 29, así sucesivamente hasta la última columna la cual contiene del 80 al 90
 //7)    Mostrar el carton por pantalla
 
-//Hay errores, como que a veces hay mas espacios en blanco de lo necesario, o mas numeros de lo necesario.
-//O como cuando los numeros por alguna razon no se ordenan correctamente de menor a mayor, generalmente cuando son 3 en una columna
-
 int[,] carton = new int[3, 9];
-int enBlanco = 0;
-int enBlancoFila = 0;
-int enNumero = 0;
 int numero = 0;
-int enNumeroTotal = 0;
-int[] numeros = new int[20];
+int[] numeros = new int[27];
 int col = carton.GetLength(1);
 int row = carton.GetLength(0);
 bool redo = false;
 Random rand = new Random();
 int agregado = 0;
-int enNumeroFila = 0;
+int ran = 0;
+int rand1;
+bool devolver = false;
 
 void Inicializar()
 {
     carton = new int[3, 9];
-    enBlanco = 0;
-    enBlancoFila = 0;
-    enNumero = 0;
     numero = 0;
-    enNumeroTotal = 0;
-    numeros = new int[20];
+    numeros = new int[27];
     agregado = 0;
-    enNumeroFila = 0;
+    ran = 0;
+    rand1 = 0;
+    devolver = false;
 }
 
-
 int Numero(int desde, int i, int suma) {
-
     do
     {
         redo = false;
-
         numero = rand.Next(desde, (i + 1) * 10 + suma);
-
         for (int x = 0; x < numeros.Length; x++)
         {
             if (numeros[x] == numero)
@@ -59,66 +47,52 @@ int Numero(int desde, int i, int suma) {
             }
         }
     } while (redo);
-
     return numero; 
 }
 
-
-
-
 void CrearCarton()
 {
-    Console.WriteLine();
-    Console.WriteLine("=======================================");
-    Console.WriteLine();
-
     Inicializar();
 
     for (int i = 0; i < col; i++)
     {
-        enBlancoFila = 0;
-        enNumeroFila = 0;
         for (int j = 0; j < row; j++)
         {
-
-            if ((rand.Next(0, 2) == 0 || enNumeroFila > 1 || enNumeroTotal >= 15) && enBlanco < 12 && enBlancoFila < 2)
+            do
             {
-                enBlanco++;
-                enBlancoFila++;
-                carton[j, i] = 0;
-                continue;
-            }
+                if (i == 0) carton[j, i] = Numero(1, i, 0);
+                if (i == col - 1) carton[j, i] = Numero(i * 10, i, 1);
+                if (i != 0 && i < col - 1) carton[j, i] = Numero(i * 10, i, 0);
+            } while (EstaEnCarton(carton[j, i]));
 
-            if (i == 0)
-            {
-                carton[j, i] = Numero(1, i, 0);
-                enNumero++;
-                AgregarNumeros(carton[j, i]);
-                continue;
-            }
-
-            if (i == col - 1)
-            {
-                carton[j, i] = Numero(i * 10, i, 1);
-                enNumero++;
-                AgregarNumeros(carton[j, i]);
-                continue;
-            }
-
-
-            carton[j, i] = Numero(i * 10, i, 0);
-            enNumero++;
-            enNumeroTotal++;
-            enNumeroFila++;
             AgregarNumeros(carton[j, i]);
         }
     }
 
-    Console.WriteLine($"Casillas en blanco: {enBlanco} y con numero: {enNumero}");
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            do
+            {
+                rand1 = rand.Next(0, 9);
+                ran = carton[i, rand1];
+                if (carton[0, rand1] == 0 && carton[1, rand1] == 0)
+                {
+                    devolver = true;
+                    continue;
+                }
+                if (ran != 0) carton[i, rand1] = 0;
+                devolver = false;
+            } while (ran == 0 || devolver);
+        }
+    }
+}
 
-    Console.WriteLine();
-    Console.WriteLine("=======================================");
-    Console.WriteLine();
+bool EstaEnCarton(int numero)
+{
+    for (int x = 0; x < numeros.Length; x++) if (numero == numeros[x]) return true;
+    return false;
 }
 
 void AgregarNumeros(int numero)
@@ -127,17 +101,14 @@ void AgregarNumeros(int numero)
     {
         numeros[agregado] = numero;
         agregado++;
-    } catch (Exception ex)
-    {
-        
-    }
+    } catch (Exception ex){}
 }
 
 int mayor = 0;
 int[] aux = new int[2];
 int aux1;
 
-void OrderarCarton()
+void OrdenarCarton()
 {
     for (int i = 0; i < col; i++)
     {
@@ -151,7 +122,7 @@ void OrderarCarton()
                 mayor = carton[j, i];
                 aux[0] = j;
                 aux[1] = i;
-            } else if (carton[j,i] < mayor)
+            } else if (carton[j, i] != 0 && carton[j,i] < mayor)
             {
                 aux1 = carton[aux[0],aux[1]];
                 carton[aux[0], aux[1]] = carton[j, i];
@@ -163,56 +134,84 @@ void OrderarCarton()
     }
 }
 
-
 void ImprimirCarton()
 {
-    //Imprime el carton
-    //Console.Write("   Rows | ");
-    //Console.WriteLine("Cols -->");
-    //Console.WriteLine("   |");
-    //Console.WriteLine("   v");
-
+    Console.WriteLine();
+    Console.WriteLine("Carton: ");
     Console.ForegroundColor = ConsoleColor.Blue;
     Console.WriteLine();
     Console.WriteLine("==============================================");
-    //Console.WriteLine();
-
-    //Console.WriteLine("   [0]   |   [1]   |   [2]   |   [3]   |   [4]   |   [5]   |   [6]   |   [7]   |   [8]   |   [9]  ");
-
     for (int i = 0; i < row; i++)
     {
-        //Console.Write($"   [{i + 1}]");
         for (int j = 0; j < col; j++)
         {
-            if (carton[i, j] == 0)
-            {
-                Console.Write($"|    ");
-                continue;
-            }
-            if (carton[i, j] >= 10)
-            {
-                Console.Write($"| {carton[i, j]} ");
-                continue;
-            }
-            Console.Write($"| 0{carton[i, j]} ");
+            Console.Write($"|");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Write($" {(carton[i, j] < 10 ? (carton[i, j] == 0 ? "♥♥" : $"0{carton[i, j]}") : carton[i, j])} ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Black;
         }
+        Console.BackgroundColor = ConsoleColor.Black;
         Console.WriteLine("|");
     }
-
     Console.WriteLine("==============================================");
-    Console.WriteLine();
     Console.ForegroundColor = ConsoleColor.White;
 }
 
+int suma;
+double[] promedios = new double[9];
+int cantRow = 0;
+
+void PromedioCol()
+{
+    promedios = new double[9];
+    for (int i = 0; i < col; i++)
+    {
+        suma = 0;
+        cantRow = 0;
+        for (int j = 0; j < row; j++)
+        {
+            if (carton[j, i] != 0)
+            {
+                suma += carton[j, i];
+                cantRow++;
+            }
+        }
+        promedios[i] = suma / cantRow;
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Los promedios son:");
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("==============================================");
+    for (int i = 0; i < promedios.Length; i++)
+    {
+        Console.Write($"|");
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.BackgroundColor = ConsoleColor.Gray;
+        Console.Write($" {(promedios[i] < 10 ? $"0{promedios[i]}" : promedios[i])} ");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.BackgroundColor = ConsoleColor.Black;
+    }
+    Console.WriteLine("|");
+    Console.WriteLine("==============================================");
+    Console.ForegroundColor = ConsoleColor.White;
+}
 
 //Llamadores principales
-
-Console.Write("Cuantos cartones de Bingo desea crear?: ");
-int cantidad = int.Parse(Console.ReadLine());
-
-for (int i = 1; i <= cantidad; i++)
+int cantidad = 0;
+do
 {
-    CrearCarton();
-    OrderarCarton();
-    ImprimirCarton();
-}
+    Console.WriteLine();
+    Console.Write("Cuantos cartones de Bingo desea crear?: ");
+    cantidad = int.Parse(Console.ReadLine());
+
+    for (int i = 1; i <= cantidad; i++)
+    {
+        CrearCarton();
+        OrdenarCarton();
+        ImprimirCarton();
+        PromedioCol();
+    }
+} while (cantidad != 0);
