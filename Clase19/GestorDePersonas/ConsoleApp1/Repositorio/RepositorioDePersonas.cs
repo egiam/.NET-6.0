aff3741ddb0c8106f45c8892f38b9ff6e4b121ce
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2022-06-30 15:14:30 -0300

    Till minute 40

--- Clase19/GestorDePersonas/ConsoleApp1/Repositorio/RepositorioDePersonas.cs
+++ Clase19/GestorDePersonas/ConsoleApp1/Repositorio/RepositorioDePersonas.cs
@@ -5,46 +5,78 @@ namespace ConsoleApp1.Repositorio
 {
     public class RepositorioDePersonas
     {
-        public List<Persona> Personas { get; set; }
+        //public List<Persona> Personas { get; set; }
+        public Dictionary<string, Persona> Personas { get; set; }
 
         public RepositorioDePersonas()
         {
-            Personas = new List<Persona>();
+            Personas = new Dictionary<string, Persona>();
         }
 
         public void Insertar(Persona persona)
         {
-            Personas.Add(persona);
+            var numeroDocumento = persona.NumeroDeDocumento;
+            var personaExiste = Personas.ContainsKey(numeroDocumento); // Verifica si el diccionario contiene la key de ese nro de doc
+
+            if (!personaExiste)
+                Personas[numeroDocumento] = persona;
+
+            //Personas.Add(persona);
         }
 
         public void Eliminar(string numeroDocumento)
         {
             //Definir como eliminar una persona de la Lista de Personas.
+
+            Personas[numeroDocumento] = null;
+            
+            //Persona personaAEliminar = null;
+            //foreach (var persona in Personas)
+            //{
+            //    if (persona.NumeroDeDocumento == numeroDocumento)
+            //    {
+            //        personaAEliminar = persona;
+            //        break;
+            //    }
+            //}
+
+            //if (personaAEliminar != null)
+            //{
+            //    Personas.Remove(personaAEliminar);
+            //}
         }
 
         public void Actualizar(Persona persona)
-        {   
-            foreach (var personaActual in Personas)
+        {
+            var personaAActualizar = Personas[persona.NumeroDeDocumento];
+
+            if (personaAActualizar != null)
             {
-                if (personaActual.NumeroDeDocumento == persona.NumeroDeDocumento)
-                {
-                    personaActual.Nombre = persona.Nombre;
-                    personaActual.Apellido = persona.Apellido;
-                    personaActual.FechaNacimiento = persona.FechaNacimiento;
-                }
+                personaAActualizar.Nombre = persona.Nombre;
+                personaAActualizar.Apellido = persona.Apellido;
+                personaAActualizar.FechaNacimiento = persona.FechaNacimiento;
             }
         }
 
         public void Actualizar(string numeroDocumento, string nombre, string apellido)
         {
-            foreach (var personaActual in Personas)
+            var personaAActualizar = Personas[numeroDocumento];
+
+            if (personaAActualizar != null)
             {
-                if (personaActual.NumeroDeDocumento == numeroDocumento)
-                {
-                    personaActual.Nombre = nombre;
-                    personaActual.Apellido = apellido;
-                }
+                personaAActualizar.Nombre = nombre;
+                personaAActualizar.Apellido = apellido;
             }
         }
+        public bool Existe(string numeroDocumento)
+        {
+            return Personas.ContainsKey(numeroDocumento);
+        }
+
+        public bool Existe(Persona persona)
+        {
+            return Existe(persona.NumeroDeDocumento);
+        }
+
     }
 }
