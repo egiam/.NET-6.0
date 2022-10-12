b3ebe63cb18e660e0f9896bc6f7e283d8c9b1804
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2022-10-12 13:35:15 -0700

    Clase44

---
+++ Clases/Clase44/ProyectoFinal/ProyectoFinal/ProyectoFinal/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoFinal.Models;
+using ProyectoFinal.Rules;
+using System.Diagnostics;
+
+namespace ProyectoFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Suerte()
+        {
+            var rule = new PublicacionRule();
+            var post = rule.GetOnePostRandom();
+            return View(post);
+        }
+        public IActionResult AcercaDe()
+        {
+            return View();
+        }
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
