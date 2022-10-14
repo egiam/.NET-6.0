f7874edfb61ae8f9b7f57b8141eeeb269d9be3c7
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2022-10-13 19:13:57 -0700

    Clase45

---
+++ Clases/Clase45/ProyectoFinal/ProyectoFinal/Rules/PublicacionRule.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using ProyectoFinal.Models;
+using System.Data.SqlClient;
+
+namespace ProyectoFinal.Rules
+{
+    public class PublicacionRule
+    {
+        public Publicacion GetOnePostRandom()
+        {
+
+            var connectionString = @"Server=.\SQL2019; Database=BlogDatabase; Trusted_Connection=True";
+            using var connection = new SqlConnection(connectionString);
+            {
+                connection.Open();
+                var posts = connection.Query<Publicacion>("SELECT TOP 1 * FROM Publicacion");
+
+                return posts.First();
+            }
+        }
+
+    }
+}
