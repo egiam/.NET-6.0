238ce75e435e1c418c1901f4059e51ac7d754d9d
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2022-10-26 12:52:21 -0700

    Clase48

---
+++ Clases/Clase48/ProyectoFinal/ProyectoFinal/ProyectoFinal/Rules/PublicacionRule.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using ProyectoFinal.Models;
+using System.Data.SqlClient;
+
+namespace ProyectoFinal.Rules
+{
+    public class PublicacionRule
+    {
+        private readonly IConfiguration _configuration;
+        public PublicacionRule(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public Publicacion GetOnePostRandom()
+        {
+            var connectionString = _configuration.GetConnectionString("BlogDatabase");
+            using var connection = new SqlConnection(connectionString);
+            {
+                connection.Open();
+                var posts = connection.Query<Publicacion>("SELECT TOP 1 * FROM Publicacion ORDER BY NEWID()");
+
+                return posts.First();
+            }
+        }
+
+        public Publicacion GetPostById(int id)
+        {
+            var connectionString = _configuration.GetConnectionString("BlogDatabase");
+            using var connection = new SqlConnection(connectionString);
+            {
+                connection.Open();
+                var query = "SELECT * FROM Publicacion WHERE Id = @id";
+                var posts = connection.QueryFirstOrDefault<Publicacion>(query,new { id });
+
+                return posts;
+            }
+        }
+
+        public List<Publicacion> GetPostsHome()
+        {
+            var connectionString = _configuration.GetConnectionString("BlogDatabase");
+            using var connection = new SqlConnection(connectionString);
+            {
+                connection.Open();
+                var posts = connection.Query<Publicacion>("SELECT TOP 4 * FROM Publicacion ORDER BY Creacion DESC");
+
+                return posts.ToList();
+            }
+        }
+
+    }
+}
